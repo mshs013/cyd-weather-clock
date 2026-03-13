5b5b52a6483914f14a57c5b75382978862e887c1
Sazzad Hossain <mshs013@gmail.com>
2026-03-13 20:10:59 +0600

    Remove LVGL unused widget dependency

--- main/ui/ui_helpers.h
+++ main/ui/ui_helpers.h
@@ -32,10 +32,6 @@ void _ui_image_set_property(lv_obj_t * target, int id, uint8_t * val);
 #define _UI_LABEL_PROPERTY_TEXT 0
 void _ui_label_set_property(lv_obj_t * target, int id, const char * val);
 
-#define _UI_ROLLER_PROPERTY_SELECTED 0
-#define _UI_ROLLER_PROPERTY_SELECTED_WITH_ANIM 1
-void _ui_roller_set_property(lv_obj_t * target, int id, int val);
-
 #define _UI_SLIDER_PROPERTY_VALUE 0
 #define _UI_SLIDER_PROPERTY_VALUE_WITH_ANIM 1
 void _ui_slider_set_property(lv_obj_t * target, int id, int val);
@@ -44,13 +40,9 @@ void _ui_screen_change(lv_obj_t ** target, lv_scr_load_anim_t fademode, int spd,
 
 void _ui_screen_delete(void (*target)(void));
 
-void _ui_arc_increment(lv_obj_t * target, int val);
-
 void _ui_bar_increment(lv_obj_t * target, int val, int anm);
 
 void _ui_slider_increment(lv_obj_t * target, int val, int anm);
-
-void _ui_keyboard_set_target(lv_obj_t * keyboard, lv_obj_t * textarea);
 
 #define _UI_MODIFY_FLAG_ADD 0
 #define _UI_MODIFY_FLAG_REMOVE 1
@@ -61,12 +53,6 @@ void _ui_flag_modify(lv_obj_t * target, int32_t flag, int value);
 #define _UI_MODIFY_STATE_REMOVE 1
 #define _UI_MODIFY_STATE_TOGGLE 2
 void _ui_state_modify(lv_obj_t * target, int32_t state, int value);
-
-#define UI_MOVE_CURSOR_UP 0
-#define UI_MOVE_CURSOR_RIGHT 1
-#define UI_MOVE_CURSOR_DOWN 2
-#define UI_MOVE_CURSOR_LEFT 3
-void _ui_textarea_move_cursor(lv_obj_t * target, int val)
 ;
 
 void scr_unloaded_delete_cb(lv_event_t * e);
@@ -114,17 +100,11 @@ int32_t _ui_anim_callback_get_image_angle(lv_anim_t * a);
 
 int32_t _ui_anim_callback_get_image_frame(lv_anim_t * a);
 
-void _ui_arc_set_text_value(lv_obj_t * trg, lv_obj_t * src, const char * prefix, const char * postfix);
-
 void _ui_slider_set_text_value(lv_obj_t * trg, lv_obj_t * src, const char * prefix, const char * postfix);
 
 void _ui_checked_set_text_value(lv_obj_t * trg, lv_obj_t * src, const char * txt_on, const char * txt_off);
 
-void _ui_spinbox_step(lv_obj_t * target, int val)
-;
-
-void _ui_switch_theme(int val)
-;
+void _ui_switch_theme(int val);
 
 #ifdef __cplusplus
 } /*extern "C"*/
